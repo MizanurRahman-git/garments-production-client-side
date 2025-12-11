ce310b109c8d4f650522de1cc0a4dda121e10d85
MizanurRahmanHridoy <mizanurrahmanhridoy42@gmail.com>
2025-12-11 21:25:01 +0600

    frontend deploy in firebase

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from "react";
 
 const axiosIntance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://garments-production-server.vercel.app",
   });
 
 const useAxios = () => {
